fb4a1dcf14ab0d2911c7a138b3c334147bc9f2db
EviNikolaidou <Evi.Nikolaidou@outlook.com>
2020-01-09 16:43:02 +0000

    readings/routes

--- crystals/application/routes.py
+++ crystals/application/routes.py
@@ -1,7 +1,7 @@
 from random import randint
  
 def crystal generator():
-    listOfCrystals = ["Dolomite", "Blue Opal", "Green Apatite", "Manganoan Calcite", "Agni Gold Danburite", "Ajoite", "Ametrine", "Astrophyllite" , "Azurite" , "Tiger's Eye", "Angelite", "Blue Lace" ]
+    listOfCrystals = ["Dolomite", "Blue Opal", "Astrophyllite" , "Azurite" , "Tiger's Eye", "Blue Lace" ]
     random_number = randint(0,len(listOfCrystals))
     print (random_number)
     crystal = listOfCrystals[random_number]
---
+++ readings/application/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template, redirect, url_for
+from application import app
+import requests
+
+@app.route("/", methods= ["GET", "POST"])
+def reading():
+    crystal_service = "http://crystal:5000/"
+    card_service = "http://card:5000/"
+     if crystal_service == "Dolomite" or "Blue Opal" or "Astrophyllite":
+         if card_service == "The Fool":
+             reading = "this is your reading"
+         else
+            reading = "this is your reading"
+     elif crystal_service == "Azurite" or "Tiger's Eye" or "Blue Lace" :
+         if card_service == "The Fool":
+             reading = "this is your reverse reading"
+         else
+            reading = "this is your reverse  reading"
+    return reading
